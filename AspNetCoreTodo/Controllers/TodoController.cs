0cab9adc651a9e837f08ba00a3584ca61fad5b1d
Cailenys <cailenys.salazar@gmail.com>
2020-05-15 08:39:34 -0600

    Truck Controller setup

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -1,4 +1,4 @@
-/*using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,11 +7,14 @@ namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index()
+        private readonly ITodoItemService _todoItemService;
+        public TodoController(ITodoItemService todoItemService)
         {
-            // Get to-do items from database
-            // Put items into a model
-            // Render view using the model
+        _todoItemService = todoItemService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
         }
     }
-}*/
+}
